7afb7618552082686f649d1dc1cc9760f3c7edc0
josephgodwinkimani <13916761+josephgodwinkimani@users.noreply.github.com>
2022-12-06 11:24:25 +0300

    Update selfsigned_fixer.sh

--- selfsigned_fixer.sh
+++ selfsigned_fixer.sh
@@ -10,6 +10,8 @@ if [[ ! -d /etc/letsencrypt/live ]]; then
 fi
 
 echo "Remove only the domain keys NOT mail or any other subdomains for the same domain"
-rm -f /etc/letsencrypt/live/$DOMAIN/privkey.pem && rm -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem
+if [[ ! -d /etc/letsencrypt/live/$DOMAIN ]]; then
+    rm -f /etc/letsencrypt/live/$DOMAIN/privkey.pem && rm -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem
+fi
 echo "Issuing SSL certificate with Let's Encrypt"
 /root/.acme.sh/acme.sh --issue -d $DOMAIN -d www.$DOMAIN --cert-file /etc/letsencrypt/live/$DOMAIN/cert.pem --key-file /etc/letsencrypt/live/$DOMAIN/privkey.pem --fullchain-file /etc/letsencrypt/live/$DOMAIN/fullchain.pem -w /usr/local/lsws/Example/html --force --debug
