b42925978aa99b3a350f2debb62e5afee481fd9b
josephgodwinkimani <13916761+josephgodwinkimani@users.noreply.github.com>
2022-12-06 22:06:19 +0300

    Create crowdsec_update.sh

---
+++ crowdsec_update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# use cscli that manages crowdsec to update
+
+sudo cscli hub update --trace
+sudo cscli hub upgrade --force --trace
+sudo cscli scenarios upgrade --all --force --trace
+sudo cscli parsers upgrade --all --force --trace
+sudo cscli collections upgrade --all --force --trace
+sudo cscli postoverflows upgrade --all --force --trace
+sudo cscli metrics
