1758dcc9c89edc94ba0148e65c9858182aff19c8
josephgodwinkimani <13916761+josephgodwinkimani@users.noreply.github.com>
2022-12-06 11:23:06 +0300

    Create selfsigned_fixer.sh

---
+++ selfsigned_fixer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Ask for domain
+echo "Which domain do you want to issue SSL certificate? (e.g mydomain.com) "
+read DOMAIN
+
+if [[ ! -d /etc/letsencrypt/live ]]; then
+    echo "Please run upgrade script - https://community.cyberpanel.net/docs?topic=81"
+    exit
+fi
+
+echo "Remove only the domain keys NOT mail or any other subdomains for the same domain"
+rm -f /etc/letsencrypt/live/$DOMAIN/privkey.pem && rm -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem
+echo "Issuing SSL certificate with Let's Encrypt"
+/root/.acme.sh/acme.sh --issue -d $DOMAIN -d www.$DOMAIN --cert-file /etc/letsencrypt/live/$DOMAIN/cert.pem --key-file /etc/letsencrypt/live/$DOMAIN/privkey.pem --fullchain-file /etc/letsencrypt/live/$DOMAIN/fullchain.pem -w /usr/local/lsws/Example/html --force --debug
