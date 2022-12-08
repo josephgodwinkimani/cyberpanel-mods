b368bc8e7a334716386a3fde3241b5d6aa7faa7c
josephgodwinkimani <13916761+josephgodwinkimani@users.noreply.github.com>
2022-12-08 16:44:00 +0300

    add centos and almalinux support

--- php_ioncube_install.sh
+++ php_ioncube_install.sh
@@ -22,34 +22,99 @@ CURRENT=$(php -v | head -n 1 | cut -d " " -f 2 | cut -f1-2 -d".")
 
 echo "current php version of this system PHP-$CURRENT"
 
+DISTRO=`cat /etc/*-release | grep "^ID=" | grep -E -o "[a-z]\w+"`
+
+echo "$DISTRO"
+
 
 if [  $CURRENT = $a ];
 then
-      cp ioncube/ioncube_loader_lin_7.2.so /usr/local/lsws/lsphp72/lib/php/20170718/
-      echo "zend_extension=ioncube_loader_lin_7.2.so" >> /usr/local/lsws/lsphp72/etc/php/7.2/mods-available/01-ioncube.ini     
-      systemctl restart lsws
-      echo "ioncube_loader_7.2 sucessfully installed"
+
+      if [ "$DISTRO" = "ubuntu" ]; then
+      
+         cp ioncube/ioncube_loader_lin_7.2.so /usr/local/lsws/lsphp72/lib/php/20170718/
+         echo "zend_extension=ioncube_loader_lin_7.2.so" >> /usr/local/lsws/lsphp72/etc/php/7.2/mods-available/01-ioncube.ini     
+         systemctl restart lsws
+         echo "ioncube_loader_7.2 sucessfully installed"
+         
+      elif [ "$DISTRO" = "centos" ] || [ "$DISTRO" = "almalinux" ]; then
+      
+        cp --verbose ioncube/ioncube_loader_lin_7.2.so /usr/local/lsws/lsphp72/lib64/php/modules/
+        echo "zend_extension=ioncube_loader_lin_7.2.so" >> /usr/local/lsws/lsphp72/etc/php.d/01-ioncube.ini
+        service lsws restart
+        echo "ioncube_loader_7.2 sucessfully installed"
+        
+      else
+         echo "$DISTRO"
+         echo "Sorry this is not for you"
+      fi
+
 
 elif [  $CURRENT = $b  ];
 then
-      cp ioncube/ioncube_loader_lin_7.3.so /usr/local/lsws/lsphp73/lib/php/20180731/
-      echo "zend_extension=ioncube_loader_lin_7.2.so" >> /usr/local/lsws/lsphp73/etc/php/7.3/mods-available/01-ioncube.ini     
-      systemctl restart lsws
-      echo "ioncube_loader_7.3 sucessfully installed"
+      
+      if [ "$DISTRO" = "ubuntu" ]; then
+      
+            cp ioncube/ioncube_loader_lin_7.3.so /usr/local/lsws/lsphp73/lib/php/20180731/
+            echo "zend_extension=ioncube_loader_lin_7.2.so" >> /usr/local/lsws/lsphp73/etc/php/7.3/mods-available/01-ioncube.ini     
+            systemctl restart lsws
+            echo "ioncube_loader_7.3 sucessfully installed"
+         
+      elif [ "$DISTRO" = "centos" ] || [ "$DISTRO" = "almalinux" ]; then
+      
+            cp --verbose ioncube/ioncube_loader_lin_7.3.so /usr/local/lsws/lsphp73/lib64/php/modules/
+            echo "zend_extension=ioncube_loader_lin_7.3.so" >> /usr/local/lsws/lsphp73/etc/php.d/01-ioncube.ini
+            service lsws restart
+            echo "ioncube_loader_7.3 sucessfully installed"
+        
+      else
+         echo "$DISTRO"
+         echo "Sorry this is not for you"
+      fi
 
 elif [  $CURRENT = $c  ];
 then
-      cp ioncube/ioncube_loader_lin_7.4.so /usr/local/lsws/lsphp74/lib/php/20190902/
-      echo "zend_extension=ioncube_loader_lin_7.4.so" >> /usr/local/lsws/lsphp74/etc/php/7.4/mods-available/01-ioncube.ini     
-      systemctl restart lsws
-      echo "ioncube_loader_7.4 sucessfully installed"
+      
+      if [ "$DISTRO" = "ubuntu" ]; then
+      
+            cp ioncube/ioncube_loader_lin_7.4.so /usr/local/lsws/lsphp74/lib/php/20190902/
+            echo "zend_extension=ioncube_loader_lin_7.4.so" >> /usr/local/lsws/lsphp74/etc/php/7.4/mods-available/01-ioncube.ini     
+            systemctl restart lsws
+            echo "ioncube_loader_7.4 sucessfully installed"
+         
+      elif [ "$DISTRO" = "centos" ] || [ "$DISTRO" = "almalinux" ]; then
+      
+            cp --verbose ioncube/ioncube_loader_lin_7.4.so /usr/local/lsws/lsphp74/lib64/php/modules/
+            echo "zend_extension=ioncube_loader_lin_7.4.so" >> /usr/local/lsws/lsphp74/etc/php.d/01-ioncube.ini
+            service lsws restart
+            echo "ioncube_loader_7.4 sucessfully installed"
+        
+      else
+         echo "$DISTRO"
+         echo "Sorry this is not for you"
+      fi
 
 elif [  $CURRENT = $d  ];
 then
-      cp ioncube/ioncube_loader_lin_8.1.so /usr/local/lsws/lsphp81/lib/php/20210902/
-      echo "zend_extension=ioncube_loader_lin_8.1.so" >> /usr/local/lsws/lsphp81/etc/php/8.1/mods-available/01-ioncube.ini     
-      systemctl restart lsws
-      echo "ioncube_loader_8.1 sucessfully installed"
+      
+      if [ "$DISTRO" = "ubuntu" ]; then
+      
+            cp ioncube/ioncube_loader_lin_8.1.so /usr/local/lsws/lsphp81/lib/php/20210902/
+            echo "zend_extension=ioncube_loader_lin_8.1.so" >> /usr/local/lsws/lsphp81/etc/php/8.1/mods-available/01-ioncube.ini     
+            systemctl restart lsws
+            echo "ioncube_loader_8.1 sucessfully installed"
+         
+      elif [ "$DISTRO" = "centos" ] || [ "$DISTRO" = "almalinux" ]; then
+      
+            cp --verbose ioncube/ioncube_loader_lin_8.1.so /usr/local/lsws/lsphp81/lib64/php/modules/
+            echo "zend_extension=ioncube_loader_lin_8.1.so" >> /usr/local/lsws/lsphp81/etc/php.d/01-ioncube.ini
+            service lsws restart
+            echo "ioncube_loader_8.1 sucessfully installed"
+        
+      else
+         echo "$DISTRO"
+         echo "Sorry this is not for you"
+      fi
 
 
 else
