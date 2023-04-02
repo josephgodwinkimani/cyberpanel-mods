930c32b6f145efb1ea2d3c990d445911a3f75f3a
josephgodwinkimani <13916761+josephgodwinkimani@users.noreply.github.com>
2023-04-02 14:23:54 +0300

    add rclone command to backup mail directories

--- rclone_backup_cronjob.sh
+++ rclone_backup_cronjob.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# RUn rclone copy all incremental backups at (/home/backups) to remote cloud e.g. gdrive, mega etc.
-# This file is dropped in by https://github.com/josephgodwinkimani/cyberpanel at /usr/local/CyberCP
+# Run rclone copy all incremental backups at (/home/backups) to remote cloud e.g. gdrive, mega etc.
+# This file is dropped in by https://github.com/josephgodwinkimani/cyberpanel at /usr/local/CyberCP or /usr/local
 
 # If you are only copying a small number of files (or are filtering most of the files) and/or have a large number of files on the destination then 
 # --no-traverse will stop rclone listing the destination and save time.
 
-rclone copy --no-traverse "/home/backup" remote:backups
+/usr/bin/rclone copy --no-traverse "/home/backups" remote:backups
+/usr/bin/rclone copy --no-traverse "/home/vmail" remote:backups
