731e034ef88efe97e1642d57d8d4bef1e6ef315a
josephgodwinkimani <13916761+josephgodwinkimani@users.noreply.github.com>
2024-12-21 16:24:34 +0300

    install_pureftpd.sh: add for all cyberpanel supported os

--- install_pureftpd.sh
+++ install_pureftpd.sh
@@ -3,22 +3,55 @@
 trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
 
 set -e
+
 log_info() {
   printf "\n\e[0;35m $1\e[0m\n\n"
 }
 
-DISTRO=`cat /etc/*-release | grep "^ID=" | grep -E -o "[a-z]\w+"`
+detect_os() {
+  if [ -f /etc/os-release ]; then
+    OS_ID=$(grep "^ID=" /etc/os-release | cut -d '=' -f 2- | tr -d '"')
+  elif [ -f /etc/*-release ]; then
+    OS_ID=$(cat /etc/*-release | grep "^ID=" | grep -E -o "[a-z]\w+")
+  fi
 
-if [ "$DISTRO" = "centos" ] || [ "$DISTRO" = "almalinux" ]; then
-   echo "Your operating system is $DISTRO"
-   echo "Sorry this is not for you"
-fi
+  case $OS_ID in
+    "ubuntu" | "debian")
+      PKG_MANAGER="apt"
+      SERVICE_MANAGER="systemctl"
+      PURE_FTPD_SERVICE="pure-ftpd"
+      ;;
+    "centos" | "almalinux" | "rhel" | "fedora")
+      PKG_MANAGER="dnf"
+      SERVICE_MANAGER="systemctl"
+      PURE_FTPD_SERVICE="pure-ftpd"
+      ;;
+    *)
+      echo "Unsupported operating system: $OS_ID"
+      exit 1
+      ;;
+  esac
+}
+
+detect_os
 
 log_info "Install pure-ftpd ..."
-sudo apt-get install pure-ftpd -y    
+if [ "$PKG_MANAGER" = "apt" ]; then
+  sudo apt update
+  sudo apt install pure-ftpd -y
+elif [ "$PKG_MANAGER" = "dnf" ]; then
+  sudo dnf install pure-ftpd -y
+fi
+
 sudo groupadd ftpgroup
 sudo useradd -g ftpgroup -d /dev/null -s /etc ftpuser
-sudo systemctl status pure-ftpd
+
+# Start and enable the service
+sudo $SERVICE_MANAGER start $PURE_FTPD_SERVICE
+sudo $SERVICE_MANAGER enable $PURE_FTPD_SERVICE
+
+# Check the service status
+sudo $SERVICE_MANAGER status $PURE_FTPD_SERVICE
 
 echo "##########################"
 echo "Pure-FTPd installed successfully"
